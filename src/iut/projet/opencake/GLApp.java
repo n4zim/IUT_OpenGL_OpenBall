7340d946877713060b9be24ffd552b8583b82468
Josua Gonzalez <jdpepi@gmail.com>
2014-10-09 16:24:30 +0200

    LA BOULE MAAAAAGIIIIIIIIIIIQUE

--- src/iut/projet/opencake/GLApp.java
+++ src/iut/projet/opencake/GLApp.java
@@ -45,20 +45,55 @@ public class GLApp {
 		//plane.set(-10, a, -10, 10, 10);
 		GL11.glRotatef(rotation, .05f, .5f, 0);	// Active la rotation (quantite, x, y, z)
 		*/
+		
+		int nbLignes = 2;
+		int nbPointsParLigne = 3;
+		sphere.MrSphere(nbLignes, nbPointsParLigne);
 	
-		sphere.MrSphere(100, 400);
-	
-		GL11.glRotatef(rotation, .05f, .5f, 0);
+		GL11.glRotatef(rotation, .05f, .2f, 0.8f);
 	    GL11.glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);	// Clear de l'écran
 			
+
+		for(int i = 0; i < sphere.points.size() - nbPointsParLigne - 1; i++) {
+			GL11.glColor3f(- 1*i, 0.8f*(1-i), 0.531f*i );
+			
+			GL11.glBegin(GL11.GL_POLYGON);
+
+			GL11.glColor3f(1,0,0);
+			Vertex p = sphere.points.get(i);
+			GL11.glVertex3f(p.x, p.y, p.z);
+
+			GL11.glColor3f(0,1,0);
+			p = sphere.points.get(i + nbPointsParLigne);
+			GL11.glVertex3f(p.x, p.y, p.z);
+
+			GL11.glColor3f(0,0,1);
+			p = sphere.points.get(i + nbPointsParLigne + 1);
+			GL11.glVertex3f(p.x, p.y, p.z);
+
+			GL11.glColor3f(1,0,1);
+			p = sphere.points.get(i + 1);
+			GL11.glVertex3f(p.x, p.y, p.z);
+			
+			GL11.glEnd();
+			
+			/*i++;
+			GL11.glVertex3f(sphere.points.get(i).x, sphere.points.get(i).y, sphere.points.get(i).z);
+			i++;
+			GL11.glVertex3f(sphere.points.get(i).x, sphere.points.get(i).y, sphere.points.get(i).z);
+			i++;*/
+			//GL11.glVertex3f(sphere.points[i].x, sphere.points[i].y, sphere.points[i].z);
+			
+		}
+		
+/*
 		GL11.glBegin(GL11.GL_POINTS);
 		GL11.glColor3f( 0.95f, 0.207f, 0.031f );
-
 		for (Vertex pt : sphere.points) {
 			GL11.glVertex3f(pt.x, pt.y, pt.z);
 		}
-		GL11.glEnd();
+		GL11.glEnd();*/
 		
 		//cube.draw();
 		//plane.draw();
