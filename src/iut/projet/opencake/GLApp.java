a657142dfac45f2734797c8f7d66985f4c1de51a
Josua Gonzalez <jdpepi@gmail.com>
2014-10-10 08:59:55 +0200

    Dessin de la sphère dans Sphere.java

--- src/iut/projet/opencake/GLApp.java
+++ src/iut/projet/opencake/GLApp.java
@@ -37,37 +37,7 @@ public class GLApp {
 		GL11.glRotatef(rotation, .05f, .3f, 0.9f); // rotation
 	    GL11.glClearColor( 1.0f, 1.0f, 1.0f, 1.0f ); // white background
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT); // Screen clear			
-
-		// Dessin de la sphère
-		for(int i = 0; i < sphere.points.size() - nbPointsParLigne - 1; i++) {
-			GL11.glBegin(GL11.GL_POLYGON); // Begin polygon drawing
-			
-			// Drawiwng order :
-			// 4 --- 1
-			// |     |
-			// 3 --- 2
-			
-			// 1, point i
-			GL11.glColor3f(1,0,0);
-			Vertex p = sphere.points.get(i);
-			GL11.glVertex3f(p.x, p.y, p.z);
-
-			// 2, point de la ligne en desssous
-			GL11.glColor3f(0,1,0);
-			p = sphere.points.get(i + nbPointsParLigne);
-			GL11.glVertex3f(p.x, p.y, p.z);
-			
-			// 3, point suivant de la ligne en dessous
-			GL11.glColor3f(0,0,1);
-			p = sphere.points.get(i + nbPointsParLigne + 1);
-			GL11.glVertex3f(p.x, p.y, p.z);
-			
-			// 4, point suivant de la même ligne que i
-			GL11.glColor3f(1,0,1);
-			p = sphere.points.get(i + 1);
-			GL11.glVertex3f(p.x, p.y, p.z);
-			
-			GL11.glEnd(); // end polygon
-		}
+		
+		sphere.Draw();
 	}
 }
--- src/iut/projet/opencake/Sphere.java
+++ src/iut/projet/opencake/Sphere.java
@@ -2,11 +2,16 @@ package iut.projet.opencake;
 
 import java.util.Vector;
 
+import org.lwjgl.opengl.GL11;
+
 public class Sphere {
 	Vector<Vertex> points = new Vector<Vertex>();
+	int nbPointsParLigne;
 	
 	public void MrSphere(int subSeg, int subPi) {
 		points = new Vector<Vertex>();
+		nbPointsParLigne = subPi;
+		
 		for (int divSeg = 0; divSeg <= subSeg; divSeg++) {
 			float t = (float) Math.PI * divSeg / subSeg;
 			
@@ -18,6 +23,40 @@ public class Sphere {
 				
 				points.addElement(new Vertex(x, y, z));
 			}
+		}
+	}
+	
+	public void Draw() {
+		// Dessin de la sphère
+		for(int i = 0; i < points.size() - nbPointsParLigne - 1; i++) {
+			GL11.glBegin(GL11.GL_POLYGON); // Begin polygon drawing
+			
+			// Drawiwng order :
+			// 4 --- 1
+			// |     |
+			// 3 --- 2
+			
+			// 1, point i
+			GL11.glColor3f(1,0,0);
+			Vertex p = points.get(i);
+			GL11.glVertex3f(p.x, p.y, p.z);
+
+			// 2, point de la ligne en desssous
+			GL11.glColor3f(0,1,0);
+			p = points.get(i + nbPointsParLigne);
+			GL11.glVertex3f(p.x, p.y, p.z);
+			
+			// 3, point suivant de la ligne en dessous
+			GL11.glColor3f(0,0,1);
+			p = points.get(i + nbPointsParLigne + 1);
+			GL11.glVertex3f(p.x, p.y, p.z);
+			
+			// 4, point suivant de la même ligne que i
+			GL11.glColor3f(1,0,1);
+			p = points.get(i + 1);
+			GL11.glVertex3f(p.x, p.y, p.z);
+			
+			GL11.glEnd(); // end polygon
 		}
 	}
 }
