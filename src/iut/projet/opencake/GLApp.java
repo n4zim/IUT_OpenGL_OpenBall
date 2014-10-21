eedf86ad707e65dd74c19a2c7d7b27ab912c0012
Josua Gonzalez <jdpepi@gmail.com>
2014-10-21 23:19:59 +0200

    Tout plein de getters et setters !

--- src/iut/projet/opencake/GLApp.java
+++ src/iut/projet/opencake/GLApp.java
@@ -8,7 +8,6 @@ import org.lwjgl.util.glu.GLU;
 public class GLApp {
 	MyDisplay display;
 	Cube cube = new Cube();
-	Plane plane = new Plane();
 	float rotation = 0.0f;
 	float a = -1f;
 	int factor = 1;
@@ -17,6 +16,8 @@ public class GLApp {
 	int nbPointsParLigne = 40;
 	int frame = 0;
 	Matrice4 matSphere = new Matrice4();
+	int c = 1;
+	int fa = 1;
 	
 	public GLApp(MyDisplay display) {
 		this.display = display; 
@@ -35,18 +36,57 @@ public class GLApp {
         sphere.translationVector.y = 5f;
 	}
 	
-	int c = 1;
-	int fa = 1;
 	public void update() {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         
-        sphere.translationVector.z = -9f;
         
-        sphere.MrSphere(10, 20);
+        sphere.MrSphere(nbLignes, nbPointsParLigne);
+        sphere.translationVector.z = -9f;
         matSphere = new Matrice4();
         //matSphere.Echelle(2f, 2f, 2f);
                 
         sphere.draw(matSphere);
         sphere.fall();
+	}
+	
+
+	public Float getBottomWall() {
+		return sphere.limiteBas;
+	}
+	
+	public void setBottomWall(Float v) {
+		sphere.limiteBas = v;
+	}
+	
+	public Float getTopWall() {
+		return sphere.limiteHaut;
+	}
+	
+	public void setTopWall(Float v) {
+		sphere.limiteHaut = v;
+	}
+
+	public Float getAnimSpeed() {
+		return sphere.speed;
+	}
+	
+	public void setAnimSpeed(Float s) {
+		sphere.speed = s;
+	}
+	
+	public Integer getNbLignes() {
+		return nbLignes;
+	}
+	
+	public void setNbLignes(Integer n) {
+		nbLignes = n;
+	}
+	
+	public Integer getNbPointsParLigne() {
+		return nbPointsParLigne;
+	}
+	
+	public void setNbPointsParLigne(Integer n) {
+		nbPointsParLigne = n;
 	}
 }
